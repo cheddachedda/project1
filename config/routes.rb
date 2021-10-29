0f7c75efd1eb5060bcac64e4f6d76e6d9c1448bf
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-30 10:52:56 +1100

    Reduce and refactor Fixtures routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
-  resources :fixtures
+
+  resources :fixtures, :only => [ :index ]
+  get '/fixtures/:round' => 'fixtures#round', as: 'round'
+  get '/fixtures/:round/:id' => 'fixtures#show', as: 'fixture'
+
   resources :teams
 end
