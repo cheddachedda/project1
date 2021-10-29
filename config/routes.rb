ee7e0f2c6e8aedada05d93144d347fa738aa89f9
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-29 20:13:40 +1100

    Condense Team routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get 'teams/index'
-  get 'teams/new'
-  get 'teams/create'
-  get 'teams/show'
-  get 'teams/edit'
-  get 'teams/update'
-  get 'teams/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :teams
 end
